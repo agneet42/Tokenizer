84e15111094ac0f57864a35646a54f844efa90ae
Agneet Chatterjee <agneet257@gmail.com>
2017-03-29 08:52:49 +0530

    Update T1.py

--- T1.py
+++ T1.py
@@ -1,4 +1,4 @@
 from nltk.tokenize import TweetTokenizer			# importing the TweetTokenizer	
-T=TweetTokenizer()									# Creating object
-s=raw_input("Enter your string")					# Taking the input
-print(T.tokenize(s))								# Printing the output
+T=TweetTokenizer()									          # Creating object
+s=raw_input("Enter your string")					    # Taking the input
+print(T.tokenize(s))								          # Printing the output
